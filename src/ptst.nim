4e9b94de449b2b8cb24c03b946cf95e32f1b411b
acnologia000 <hridyanshbluedevil@hotmail.com>
2022-04-12 10:22:25 +0530

    d3

--- src/ptst.nim
+++ src/ptst.nim
@@ -1,13 +1,21 @@
 import pixie
 import json
 
+const 
+    PFP_DEMENSION = 64
+    IMAGE_MAT_SCALE = 0.1
 
-let canvasConfig = parseJson(readFile("canvas.json"))
-let messageBoxConfig = parseJson(readFile("box.json"))
+
+let
+    messageBoxColor = rgba(19, 15, 28, 255)
+
+let 
+    canvasConfig = parseJson(readFile("canvas.json"))
+    messageBoxConfig = parseJson(readFile("box.json"))
 
 var 
     pfpCFG = parseJson(readFile("pfp.json"))
-    fpfp = newImage(128,128)
+    fpfp = newImage(PFP_DEMENSION, PFP_DEMENSION)
 
 let image = newImage(canvasConfig["w"].getInt(), canvasConfig["h"].getInt())
 
@@ -28,7 +36,9 @@ let
     pfpPaint = newPaint(ImagePaint)
 
 pfpPaint.image = readImage("pfp.jpg")
+pfpPaint.imageMat = scale(vec2(IMAGE_MAT_SCALE, IMAGE_MAT_SCALE))
 pfpCTX.fillStyle = pfpPaint
+
 pfpCTX.fillCircle(
     circle(
         vec2(
@@ -44,13 +54,14 @@ let
     pos = vec2(float32(messageBoxConfig["px"].getFloat()), float32(messageBoxConfig["py"].getFloat()))
     wh = vec2(float32(messageBoxConfig["w"].getFloat()), float32(messageBoxConfig["h"].getFloat()))
     
-
+mainCTX.fillStyle = messageBoxColor
 mainCTX.fillRoundedRect(rect(pos, wh),22)
+
 mainCTX.drawImage(
     fpfp,
     float32(pfpCFG["dx"].getFloat()), # postion on x axis
     float32(pfpCFG["dy"].getFloat()), # postion on y axis
-    float32(pfpCFG["dw"].getFloat()), # width
-    float32(pfpCFG["dh"].getFloat())  # height
+    float32(PFP_DEMENSION),           # width
+    float32(PFP_DEMENSION)            # height
 )
 image.writeFile("square.png")
